424c54a55803ff2bd94a919f1b8913e9f019e6a4
r1y9o80 <Ryo-official@outlook.jp>
2025-04-19 04:48:28 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import { RecoilRoot } from "recoil";
 import { useRecoilValue } from "recoil";
-import { sectionState } from "./states/section.ts"
-import Setting from "./setting/setting"
+import { sectionState } from "./states/section.ts";
+import Setting from "./setting/setting";
 
 function App() {
   const section: string = useRecoilValue(sectionState);
 
   return (
     <>
-      {section === "setting" && <Setting/>}
+      {section === "setting" && <Setting />}
     </>
-  )
+  );
 }
 
-export default App
+// Wrap the App component with RecoilRoot
+export default function AppWithRecoilRoot() {
+  return (
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  );
+}
