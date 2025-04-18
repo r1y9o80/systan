51a7a32899d24ce3bfa28d91a2b8b93ead5d540a
r1y9o80 <Ryo-official@outlook.jp>
2025-04-19 04:55:37 +0900

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/r1y9o80/systan',  // Replace with your actual repository name
 })
