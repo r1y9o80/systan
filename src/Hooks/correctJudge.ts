69998abe8f59ce22598f19895fe413c0ff28f2c3
r1y9o80 <Ryo-official@outlook.jp>
2025-04-25 00:09:26 +0900

    a

--- src/Hooks/correctJudge.ts
+++ src/Hooks/correctJudge.ts
@@ -2,12 +2,12 @@
 export const useCorrectJudge = (correctIdx: number, inputIdx: number, setScreenState: React.Dispatch<React.SetStateAction<string>>) => {
     if(correctIdx === inputIdx){
         setScreenState("ConfirmedTrue")
-        const audio = new Audio("/KuizuData/correct.mp3")
+        const audio = new Audio("/mp/correct.mp3")
         audio.play()
     }
     else{
         setScreenState("ConfirmedFalse")
-        const audio = new Audio("/KuizuData/incorrect.mp3")
+        const audio = new Audio("/mp/incorrect.mp3")
         audio.play()
     }
 }
