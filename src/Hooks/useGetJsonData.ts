bca4fe922e739234074dcc08471b323ca74a7869
r1y9o80 <Ryo-official@outlook.jp>
2025-04-25 00:01:12 +0900

    a

--- src/Hooks/useGetJsonData.ts
+++ src/Hooks/useGetJsonData.ts
@@ -3,7 +3,7 @@ import { TypeMixData } from "../types/Quiz";
 export const useGetJsonData = async (dataName: string, startItem: number, perItem: number, sendMixData: (data: TypeMixData) => void) => {
   console.log("useGetJsonData is runnned");
   try {
-    const response = await fetch('https://r1y9o80-quiz-app.web.app/KuizuData/systan-data.json');
+    const response = await fetch(`/KuizuData/${dataName}.json`);
     const jsondata = await response.json(); // JSONとしてデータを解析
     console.log(jsondata); // データをコンソールに出力    
     const mixData = Object.fromEntries(
