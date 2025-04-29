97b6a010bcff9c1aff3868b268a2b2f2e429b721
r1y9o80 <Ryo-official@outlook.jp>
2025-04-30 02:42:21 +0900

    a

--- src/result/result.tsx
+++ src/result/result.tsx
@@ -1,10 +1,12 @@
 import './result.scss';
+import { useState } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { QuizResultState } from '../states/QuizResult';
 import { sectionState } from '../states/section';
 import type { TypeResultData, TypeResult } from '../types/Quiz_Result';
 
 export const Result = () => {
+    const [showOnlyWrong, setShowOnlyWrong] = useState(false)
     const QuizResult = useRecoilValue<TypeResultData>(QuizResultState)
     const setSection = useSetRecoilState(sectionState)
     const {data, result, CorrectPercentage} = QuizResult
@@ -12,10 +14,11 @@ export const Result = () => {
 
     const list_retrun = (QuestionNumber: number,inputKey:string, correctKey: string) =>{
         if(inputKey == correctKey){
+            if(showOnlyWrong) return
             return(
                 <>
-                  {/* <li className='resultLi'><h2 className='QuestionNumber' >{QuestionNumber}　</h2><p className='resultP'>　{data[correctKey][0]}</p></li>
-                  <li className='resultLi'><h2 className='QuestionNumber' style={{color: "blue"}}>✓　</h2><p className='resultP'>　{data[inputKey][1]}</p></li> */}
+                  <li className='resultLi'><h2 className='QuestionNumber' >{QuestionNumber}　</h2><p className='resultP'>　{data[correctKey][0]}</p></li>
+                  <li className='resultLi'><h2 className='QuestionNumber' style={{color: "blue"}}>✓　</h2><p className='resultP'>　{data[inputKey][1]}</p></li>
                 </>
             )
         }else{
@@ -34,6 +37,11 @@ export const Result = () => {
             <div id="ResultBody">
                 <div key={"resultgraf"} className="pie-chart-1" style={{backgroundImage: `conic-gradient(#2589d0 ${CorrectPercentage}%,rgb(245, 100, 100) ${CorrectPercentage}% 100%)`}}>
                     <span>{`${CorrectPercentage}%`}</span>
+                </div>
+                <div style={{ marginBottom: '20px' }}>
+                    <label className="toggle-button-1">
+                        <input type="checkbox" checked={showOnlyWrong} onChange={() => setShowOnlyWrong(prev => !prev)}/>
+                    </label>
                 </div>
                 {result.map((ele: TypeResult,i) => {
                     return(
