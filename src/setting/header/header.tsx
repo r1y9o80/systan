ce85f4c89043b7149dde98c5385c248461b9cb19
r1y9o80 <Ryo-official@outlook.jp>
2025-05-11 03:29:16 +0900

    a

--- src/setting/header/header.tsx
+++ src/setting/header/header.tsx
@@ -40,45 +40,7 @@ export const Header: React.FC<{ selectedKey: string, setSelectedKey: React.Dispa
             {stageKey}
           </li>
         ))}
-        <li style={{color:"blue"}}onClick={() => test()}>タッチ</li>
       </ul>
     </header>
   );
 });
-
-
-
-
-import { getFirestore, doc, setDoc, updateDoc, getDoc } from "firebase/firestore";
-
-async function test() {
-  const db = getFirestore();
-  const userId = auth.currentUser?.uid;
-
-  if (!userId) {
-    console.log("ユーザーがサインインしていません");
-    return;
-  }
-
-  //データベースが存在するポインタ(URL的な参照場所のこと)を取得
-  const userDocRef = doc(db, "users", userId);
-  //実際にデータを取得
-  const docSnap = await getDoc(userDocRef)
-
-  const newArray = [10, 90, 0, 50, 20, 99, 100, 0, 10, 50, 30, 50, 10, 30, 40, 20, 10, 10, 10, 0, 0, 0, 0, 0, 0, 0];
-
-  try {
-    if (!docSnap.exists()) {
-      // ドキュメントがなければ作成
-      await setDoc(userDocRef, { "1": newArray });
-      console.log("新しいユーザーのドキュメントを作成しました");
-      console.log("配列に要素が登録/更新されました");
-    } else {
-      await updateDoc(userDocRef, { "1": newArray });
-      console.log("ドキュメントはすでに存在します");
-      console.log("配列に要素が登録/更新されました");
-    }
-  } catch (error) {
-    console.error("エラーが発生しました: ", error);
-  }
-}
