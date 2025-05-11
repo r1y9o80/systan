eb763156aed3932bacd0021827fdb1945d82bf64
r1y9o80 <Ryo-official@outlook.jp>
2025-05-12 08:04:58 +0900

    a

--- src/Hooks/correctJudge.ts
+++ src/Hooks/correctJudge.ts
@@ -1,17 +1,20 @@
 import { RefObject } from "react"
 
+
+const audio1 = new Audio("/mp/correct.mp3")
+const audio2 = new Audio("/mp/incorrect.mp3")
+
+
 //ボタンが押されたときに正誤を判断
 export const useCorrectJudge = (correctIdx: string, inputIdx: string, setScreenState: React.Dispatch<React.SetStateAction<string>>, sumOfCorrect: RefObject<number>) => {
     if ('speechSynthesis' in window) window.speechSynthesis.cancel();
     if(correctIdx === inputIdx){
         setScreenState("ConfirmedTrue")
         sumOfCorrect.current +=1
-        const audio = new Audio("/mp/correct.mp3")
-        audio.play()
+        audio1.play()
     }
     else{
         setScreenState("ConfirmedFalse")
-        const audio = new Audio("/mp/incorrect.mp3")
-        audio.play()
+        audio2.play()
     }
 }
