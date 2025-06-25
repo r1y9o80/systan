ae5b319b7c81ea0b15350da96ce8eb37040f6e31
r1y9o80 <Ryo-official@outlook.jp>
2025-06-26 01:52:07 +0900

    a

--- src/Hooks/generateOccurrenceRate.ts
+++ src/Hooks/generateOccurrenceRate.ts
@@ -3,7 +3,7 @@ export const useSetOccurrencerate = (
     result_log: Record<string, {"occurrenceRate": number, "corrected": number}>
 ) => {
     for(let i=0; i<StageKeys.length; i++){
-        if(result_log[StageKeys[i]]) continue
+        if(result_log[StageKeys[i]] && result_log[StageKeys[i]]["occurrenceRate"] && result_log[StageKeys[i]]["corrected"]) continue
         result_log[StageKeys[i]] = {"occurrenceRate": 1, "corrected": 0}
     }
     return 
